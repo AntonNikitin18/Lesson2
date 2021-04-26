cbb87d66434a86c208e8f0c81f9f70b611eab533
AntonNikitin18 <82111088+AntonNikitin18@users.noreply.github.com>
2021-04-26 19:32:55 +0300

    Add files via upload

---
+++ Lesson2.java
@@ -0,0 +1,43 @@
+package com.metanit;
+
+public class Lesson2 {
+
+    public static void lesson() {
+        int a = 000; // 0
+        int b = 1010; //
+        int c = 1; //
+        int d = 1000; //
+        int e = 110011; //
+        int f = 1111111; //
+        int g = 11111111; //
+        System.out.println(Integer.toString(a, 2));
+        System.out.println(Integer.toString(b, 2));
+        System.out.println(Integer.toString(c, 2));
+        System.out.println(Integer.toString(d, 2));
+        System.out.println(Integer.toString(e, 2));
+        System.out.println(Integer.toString(f, 2));
+        System.out.println(Integer.toString(g, 2));
+        //
+        Integer h = 44;
+        Integer i = 777;
+        Integer j = 2342;
+        Integer k = 655;
+        Integer l = 52;
+        Integer m = 5;
+        Integer n = 22222;
+        String jan = Integer.toBinaryString(h);
+        String feb = Integer.toBinaryString(i);
+        String mar = Integer.toBinaryString(j);
+        String apr = Integer.toBinaryString(k);
+        String may = Integer.toBinaryString(l);
+        String jun = Integer.toBinaryString(m);
+        String jul = Integer.toBinaryString(n);
+        System.out.println(jan);
+        System.out.println(feb);
+        System.out.println(mar);
+        System.out.println(apr);
+        System.out.println(may);
+        System.out.println(jun);
+        System.out.println(jul);
+    }
+}
